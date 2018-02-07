2eac01394169d6e93d835ae356dceb555e51c277
Daniel A.C. Martin <github@daniel-martin.co.uk>
2018-02-07 17:51:56 +0000

    Reinstate DOB filtering on marriages

--- src/model/marriage_registration_v1.js
+++ src/model/marriage_registration_v1.js
@@ -6,17 +6,21 @@ const documentStore = require('../lib/document-store');
 const model = documentStore('marriage_registration_v1', [
   'bride_forenames',
   'bride_surname',
+  'bride_date_of_birth',
   'groom_forenames',
-  'groom_surname'
+  'groom_surname',
+  'groom_date_of_birth'
 ]);
 
 module.exports = {
   read: model.read,
   search: terms => model.search({
     brideForenames: terms.forenames,
-    brideSurname: terms.surname
+    brideSurname: terms.surname,
+    brideDateOfBirth: terms.dateOfBirth
   }).then(r => model.search({
     groomForenames: terms.forenames,
-    groomSurname: terms.surname
+    groomSurname: terms.surname,
+    groomDateOfBirth: terms.dateOfBirth
   }).then(r2 => r.concat(r2)))
 };
