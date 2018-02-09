545eed09258197d9ff66ba9ae584108b0688692d
Daniel A.C. Martin <github@daniel-martin.co.uk>
2018-02-09 19:36:26 +0000

    Add more unit tests and add code coverage analysis

--- test/unit/lib/db.js
+++ test/unit/lib/db.js
@@ -1,9 +1,94 @@
 'use strict';
 
-const db = require('../../../src/lib/db.js');
+const pgp = require('pg-promise');
+const proxyquire = require('proxyquire');
 const moment = require('moment');
 
+const anyStub = sinon.stub();
+const noneStub = sinon.stub();
+const oneStub = sinon.stub();
+const oneOrNoneStub = sinon.stub();
+
+const pgpMock = () => ({
+  any: anyStub,
+  none: noneStub,
+  one: oneStub,
+  oneOrNone: oneOrNoneStub
+});
+
+const db = proxyquire('../../../src/lib/db.js', { 'pg-promise': (x) => Object.assign(pgpMock, pgp(x)) });
+
 describe('lib/db.js', () => {
+  describe('any()', () => {
+    it('is a function', () => (typeof db.any).should.equal('function'));
+    it('takes two arguments', () => db.any.length.should.equal(2));
+
+    describe('When called with two arguments', () => {
+      const q = 'foo';
+      const v = 'bar';
+
+      before(() => {
+        db.any(q, v);
+      });
+
+      it('Calls the parent library', () => anyStub.should.have.been.called);
+      it('Calls the parent library with the same arguments', () => anyStub.should.have.been.calledWith(q, v));
+    });
+  });
+
+  describe('none()', () => {
+    it('is a function', () => (typeof db.none).should.equal('function'));
+    it('takes two arguments', () => db.none.length.should.equal(2));
+
+    describe('When called with two arguments', () => {
+      const q = 'foo';
+      const v = 'bar';
+
+      before(() => {
+        db.none(q, v);
+      });
+
+      it('Calls the parent library', () => noneStub.should.have.been.called);
+      it('Calls the parent library with the same arguments', () => noneStub.should.have.been.calledWith(q, v));
+    });
+  });
+
+  describe('one()', () => {
+    it('is a function', () => (typeof db.one).should.equal('function'));
+    it('takes three arguments', () => db.one.length.should.equal(3));
+
+    describe('When called with three arguments', () => {
+      const q = 'foo';
+      const v = 'bar';
+      const cb = 'baz';
+
+      before(() => {
+        db.one(q, v, cb);
+      });
+
+      it('Calls the parent library', () => oneStub.should.have.been.called);
+      it('Calls the parent library with the same arguments', () => oneStub.should.have.been.calledWith(q, v, cb));
+    });
+  });
+
+  describe('oneOrNone()', () => {
+    it('is a function', () => (typeof db.oneOrNone).should.equal('function'));
+    it('takes three arguments', () => db.oneOrNone.length.should.equal(3));
+
+    describe('When called with three arguments', () => {
+      const q = 'foo';
+      const v = 'bar';
+      const cb = 'baz';
+
+      before(() => {
+        db.oneOrNone(q, v, cb);
+      });
+
+      it('Calls the parent library', () => oneOrNoneStub.should.have.been.called);
+      it('Calls the parent library with the same arguments', () => oneOrNoneStub.should.have.been.calledWith(q, v, cb));
+    });
+  });
+
   describe('processTerms()', () => {
     it('is a function', () => (typeof db.processTerms).should.equal('function'));
     it('takes one argument', () => db.processTerms.length.should.equal(1));
