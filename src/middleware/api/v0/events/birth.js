8405081ec2ee0b1fddf8a6d11360985513933c7a
Ben Marvell <madmarv187@gmail.com>
2018-09-25 16:59:17 +0100

    Report on the client used to access the API

--- src/middleware/api/v0/events/birth.js
+++ src/middleware/api/v0/events/birth.js
@@ -57,6 +57,7 @@ module.exports = {
         .then(r => {
           if (r) {
             client.increment('lev.api.birth');
+            client.increment(`lev.api.${req.headers['x-auth-aud']}.birth`);
             res.send(censorRecord(r));
             next();
           } else {
@@ -93,6 +94,7 @@ module.exports = {
           }))
           .then(r => {
             client.increment('lev.api.birth.search');
+            client.increment(`lev.api.${req.headers['x-auth-aud']}.birth.search`);
             res.send(r.map(censorRecord));
             next();
           })
