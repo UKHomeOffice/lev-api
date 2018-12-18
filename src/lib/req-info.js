80824b13c5d91d1be45cb8f6c4bae6df26e912cc
Daniel A.C. Martin <github@daniel-martin.co.uk>
2018-12-18 17:49:21 +0000

    Add a user's roles to the metrics

--- src/lib/req-info.js
+++ src/lib/req-info.js
@@ -3,5 +3,6 @@
 module.exports = (req) => ({
   username: req.headers['x-auth-username'],
   client: req.headers['x-auth-aud'],
-  groups: req.headers['x-auth-groups'] && String(req.headers['x-auth-groups']).split(',') || []
+  groups: req.headers['x-auth-groups'] && String(req.headers['x-auth-groups']).split(',') || [],
+  roles: req.headers['x-auth-roles'] && String(req.headers['x-auth-roles']).split(',') || []
 });
--- src/middleware/v1/registration/birth.js
+++ src/middleware/v1/registration/birth.js
@@ -33,7 +33,7 @@ module.exports = {
         .then(r => {
           if (r) {
             res.send(r);
-            metrics.lookup('birth', ri.username, ri.client, ri.groups, startTime, moment(), id);
+            metrics.lookup('birth', ri.username, ri.client, ri.groups, ri.roles, startTime, moment(), id);
             next();
           } else {
             next(new errors.NotFoundError());
@@ -71,7 +71,7 @@ module.exports = {
           .then(() => model.search(query))
           .then(r => {
             res.send(r);
-            metrics.search('birth', ri.username, ri.client, ri.groups, startTime, moment(), req.query);
+            metrics.search('birth', ri.username, ri.client, ri.groups, ri.roles, startTime, moment(), req.query);
             next();
           })
           .catch(promiseRejectionHandler(next));
