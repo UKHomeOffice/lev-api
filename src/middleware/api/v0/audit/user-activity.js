27d658a0d2bf14ba9838eabf4e0f398515d9ee1f
Daniel A.C. Martin <github@daniel-martin.co.uk>
2019-03-12 18:41:31 +0000

    [LEVWEB-1213] Expose new audit fields externally

--- src/middleware/api/v0/audit/user-activity.js
+++ src/middleware/api/v0/audit/user-activity.js
@@ -12,13 +12,16 @@ module.exports = {
     const user = req.query.user;
     const from = moment(req.query.from || '1970-01-01', dateFormat);
     const to = req.query.to ? moment(req.query.to, dateFormat) : moment();
+    const group = req.query.group;
+    const operation = req.query.operation;
+    const dataset = req.query.dataset;
 
     if (!from.isValid()) {
       next(new errors.BadRequestError(`Invalid 'from' date: '${req.query.from}', please use ISO format - e.g. from=2000-01-31`));
     } else if (!to.isValid()) {
       next(new errors.BadRequestError(`Invalid 'to' date: '${req.query.to}', please use ISO format - e.g. to=2000-01-31`));
     } else {
-      model.search(from.format(dateFormat), to.format(dateFormat), user)
+      model.search(from.format(dateFormat), to.format(dateFormat), user, group, operation, dataset)
         .then(r => {
           res.send(r);
           next();
