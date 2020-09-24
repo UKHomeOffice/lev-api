1d388d268cc289d1014f0ce09e2da38bdf099c49
Aaron Gill <aaron.gill@digital.homeoffice.gov.uk>
2020-09-24 17:26:18 +0100

    [LEVWEB-1393] Updated after review comments

--- src/lib/redaction.js
+++ src/lib/redaction.js
@@ -3,67 +3,21 @@ const redactDeath = (death, roles) => {
     {
       id: death.id,
       date: death.date,
-      entryNumber: death.entryNumber,
-      inquestDate: death.inquestDate,
       deceased: {
         forenames: death.deceased.forenames,
         surname: death.deceased.surname,
         dateOfBirth: death.deceased.dateOfBirth,
         dateOfDeath: death.deceased.dateOfDeath,
         dateOfDeathQualifier: death.deceased.dateOfDeathQualifier,
-        ageAtDeath: death.deceased.ageAtDeath,
-        birthplace: death.deceased.birthplace,
-        deathplace: death.deceased.deathplace,
-        causeOfDeath: death.deceased.causeOfDeath,
-        certifiedBy: death.deceased.certifiedBy,
         sex: death.deceased.sex,
         address: death.deceased.address,
-        occupation: death.deceased.occupation,
-        relationshipToPartner: death.deceased.relationshipToPartner,
-        retired: death.deceased.retired,
-        maidenSurname: death.deceased.maidenSurname,
-        aliases: death.deceased.aliases.map((e) => {
-          return {
-            type: e.type,
-            prefix: e.prefix,
-            forenames: e.forenames,
-            surname: e.surname,
-            suffix: e.suffix
-          }
-        })
-      },
-      partner: {
-        name: death.partner.name,
-        occupation: death.partner.occupation,
-        retired: death.partner.retired
-      },
-      father: {
-        name: death.father.name,
-        occupation: death.father.occupation
-      },
-      mother: {
-        name: death.mother.name,
-        occupation: death.mother.occupation
-      },
-      informant: {
-        forenames: death.informant.forenames,
-        surname: death.informant.surname,
-        address: death.informant.address,
-        qualification: death.informant.qualification,
-        signature: death.informant.signature
+        aliases: death.deceased.aliases
       },
       registrar: {
-        signature: death.registrar.signature,
-        designation: death.registrar.designation,
         subdistrict: death.registrar.subdistrict,
         district: death.registrar.district,
         administrativeArea: death.registrar.administrativeArea
       },
-      coroner: {
-        name: death.coroner.name,
-        designation: death.coroner.designation,
-        area: death.coroner.area
-      }
     }
 }
 
--- test/unit/lib/redaction.js
+++ test/unit/lib/redaction.js
@@ -95,31 +95,20 @@ const deathData = {
 const redactedData = {
   id: 1000,
   date: "2020-09-23",
-  entryNumber: 0,
-  inquestDate: "2020-09-23",
   deceased: {
     forenames: "Forename1 Forname2",
     surname: "SURNAME",
     dateOfBirth: "2020-09-23",
     dateOfDeath: "2020-09-23",
     dateOfDeathQualifier: "On or about",
-    ageAtDeath: 20,
-    birthplace: "Birth Address",
-    deathplace: "Death Address",
     sex: "Indeterminate",
     address: "Current Address",
-    occupation: "",
-    retired: false,
-    maidenSurname: null,
-    causeOfDeath: "Natural causes",
-    certifiedBy: "Certification",
-    relationshipToPartner: "Bit on side",
-    aliases: [{
-      type: null,
-      prefix: null,
-      forenames: null,
-      surname: null,
-      suffix: null
+      aliases: [{
+        type: null,
+        prefix: null,
+        forenames: null,
+        surname: null,
+        suffix: null
     }, {
       type: null,
       prefix: null,
@@ -128,37 +117,10 @@ const redactedData = {
       suffix: null
     }]
   },
-  informant: {
-    forenames: "Informant Forename1 Informant Forename2",
-    surname: "Informant Surname",
-    address: "Informant Address",
-    qualification: "Confidential Informant",
-    signature: "Here is a signature"
-  },
   registrar: {
-    signature: "Registrar signature",
-    designation: "Registrar",
     subdistrict: "Subdistrict town",
     district: "District city",
     administrativeArea: "Adminshire"
-  },
-  coroner: {
-    name: "Informy McInformface",
-    designation: "Coroner",
-    area: "Adminshire"
-  },
-  partner: {
-    name: "Partner name",
-    occupation: "Partner occupation",
-    retired: true
-  },
-  father: {
-    name: "Father name",
-    occupation: "Father Occupation"
-  },
-  mother: {
-    name: "Mother name",
-    occupation: "Mother occupation"
   }
 }
 
@@ -168,10 +130,18 @@ describe('lib/redaction.js', () => {
       expect(redactDeath).to.be.a('function')
     })
     it('should return the death data if role has full access', () => {
-      expect(redactDeath(deathData, roleWithFullAccess)).to.deep.equal(deathData);
+      expect(redactDeath(Object.assign({}, deathData), roleWithFullAccess)).to.deep.equal(deathData);
     })
     it('should return redacted death data if role has not got full access', () => {
-      expect(redactDeath(deathData, roleWithoutFullAccess)).to.deep.equal(redactedData);
+      expect(redactDeath(Object.assign({}, deathData), roleWithoutFullAccess)).to.deep.equal(redactedData);
+    })
+    it('should not have a coroner property',() => {
+      expect(redactDeath(Object.assign({}, deathData), roleWithoutFullAccess))
+        .not.to.have.property('coroner');
+    })
+    it('should only have the following keys',() => {
+      expect(redactDeath(Object.assign({}, deathData), roleWithoutFullAccess))
+        .to.have.keys('date', 'deceased', 'id', 'registrar')
     })
   })
 })
