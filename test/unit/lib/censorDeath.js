ae05adc662f24fc36abc55e216fde5908083dead
Aaron Gill <aaron.gill@digital.homeoffice.gov.uk>
2020-10-08 13:55:13 +0100

    [LEVWEB-1393] Bump version and rename test func

--- test/unit/lib/censorDeath.js
+++ test/unit/lib/censorDeath.js
@@ -1,5 +1,5 @@
 'use strict';
-const redactDeath = require('../../../src/lib/censorDeath');
+const censorDeath = require('../../../src/lib/censorDeath');
 const roleWithFullAccess = ['full-details'];
 const roleWithoutFullAccess = ['births'];
 
@@ -242,25 +242,25 @@ const redactedDataNotBlocked = {
 describe('lib/censorDeath.js', () => {
   describe('censorDeath function', () => {
     it('should be a function', () => {
-      expect(redactDeath).to.be.a('function')
+      expect(censorDeath).to.be.a('function')
     })
     describe('record not blocked', () => {
       it('should return the full death data if role has full access', () => {
-        expect(redactDeath(Object.assign({}, deathData), roleWithFullAccess))
+        expect(censorDeath(Object.assign({}, deathData), roleWithFullAccess))
           .to.deep.equal(deathData);
       })
       it('should return redacted death data if role has not got full access', () => {
-        expect(redactDeath(Object.assign({}, deathData), roleWithoutFullAccess))
+        expect(censorDeath(Object.assign({}, deathData), roleWithoutFullAccess))
           .to.deep.equal(redactedDataNotBlocked);
       })
     })
     describe('record blocked', () => {
       it('should return null on relevant blocked record fields, with a role that has full access', () => {
-        expect(redactDeath(Object.assign({}, deathDataBlockedRecord), roleWithFullAccess))
+        expect(censorDeath(Object.assign({}, deathDataBlockedRecord), roleWithFullAccess))
           .to.deep.equal(blockedDeathData);
       })
       it('should return null on relevant blocked record fields, with a role that does not have full access', () => {
-        expect(redactDeath(Object.assign({}, deathDataBlockedRecord), roleWithoutFullAccess))
+        expect(censorDeath(Object.assign({}, deathDataBlockedRecord), roleWithoutFullAccess))
           .to.deep.equal(blockedDeathData);
       })
     })
