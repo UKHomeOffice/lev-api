98f499e03c93e0ad8bee06ad20130229b7a4acc3
aarongill1 <aaron.gill@digital.homeoffice.gov.uk>
2023-05-31 15:42:24 +0100

    Added file read of vault generated password for testing

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 'use strict';
 
 const packageJson = require('./package.json');
+const fs = require('fs');
 
 const defaultsFalse = v => String(v || '').match(/(true|yes|on)/i) !== null;
 const defaultsTrue = v => String(v || '').match(/(false|no|off)/i) === null;
+
+const dbPassword = fs.readFile('/app/config/dbpassword.txt', 'utf8', (err, data) => {
+  if (err) { console.log('file not found') };
+  return data;
+});
 
 module.exports = {
   env: process.env.NODE_ENV,
@@ -17,7 +23,7 @@ module.exports = {
   postgres: {
     host: process.env.POSTGRES_HOST || 'localhost',
     name: process.env.POSTGRES_DB,
-    pass: process.env.POSTGRES_PASSWORD,
+    pass: process.env.POSTGRES_PASSWORD || dbPassword,
     port: process.env.POSTGRES_PORT || 5432,
     user: process.env.POSTGRES_USER,
     ssl: defaultsTrue(process.env.POSTGRES_SSL)
