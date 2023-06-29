66592b2774a1d74c7f9c7c635f9848dd43652357
aarongill1 <aaron.gill@digital.homeoffice.gov.uk>
2023-06-29 16:28:21 +0100

    Tidy code

--- src/lib/generateToken.js
+++ src/lib/generateToken.js
@@ -1,16 +1,16 @@
 const config = require('../../config');
 const { Signer } = require('@aws-sdk/rds-signer');
 const { fromNodeProviderChain } = require('@aws-sdk/credential-providers');
+
 const credentials = fromNodeProviderChain();
 
 const getToken = async () => {
-
   const signer = new Signer({
     hostname: config.postgres.host,
     port: config.postgres.port,
     username: config.postgres.name,
     region: config.postgres.region,
-    credentials: credentials || { accessKeyId: 'local', secretAccessKey: 'local' }
+    credentials: credentials
   });
   return await signer.getAuthToken();
 }
