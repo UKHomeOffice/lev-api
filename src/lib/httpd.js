4fa79ef85996fd34f41e4d8a2c342327bedc87a4
Mehar Chauhan <mehar.chauhan1@digital.homeoffice.gov.uk>
2023-07-20 11:15:48 +0100

    ensuring that libs can still access httpd.log from logger module moving modified stream to httpd module to ensure stream catches all flows where httpd is used in lev-api

--- src/lib/httpd.js
+++ src/lib/httpd.js
@@ -2,8 +2,45 @@
 
 const config = require('../../config.js');
 const restify = require('lev-restify');
+const bunyan = require('bunyan');
 const httpd = restify.createServer({
   name: config.name
+});
+
+
+// disable default bunyan stream to stdout
+httpd.log.level(bunyan.FATAL + 1);
+
+function modifiedStream() {
+  return {
+    write: entry => {
+      const logObject = JSON.parse(entry)
+      // new logging elements
+      logObject['@timestamp'] = logObject.time;
+      logObject['log.level'] = bunyan.nameFromLevel[logObject.level];
+
+      // removing logging elements not required
+      delete logObject.level;
+      delete logObject.v;
+
+      process.stdout.write(JSON.stringify(logObject) + '\n');
+      const fs = require('fs');
+      const loggingDir = './logs';
+      if (!fs.existsSync(loggingDir)) {
+        fs.mkdirSync(loggingDir);
+      }
+      fs.appendFile(loggingDir + '/lev-api.log', JSON.stringify(logObject) + '\n', err => {
+        if (err) {
+          console.error(err);
+        }
+      });
+    }
+  };
+};
+
+httpd.log.addStream({
+  level: 'debug',
+  stream: modifiedStream(),
 });
 
 module.exports = httpd;
