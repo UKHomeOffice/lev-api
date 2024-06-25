77664e96a1f229630bbd9f0d06559ca8b401f1c4
Mehar Chauhan <mehar.chauhan1@digital.homeoffice.gov.uk>
2024-06-25 11:06:36 +0100

    removing both ./src/* and ./test/* from eslint ignore list

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,15 +3,15 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default [
-  { 
-    files: ["**/*.js"], 
-    languageOptions: { 
-      sourceType: "script", 
-      globals: globals.browser 
-    } 
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "script",
+      globals: globals.browser
+    }
   },
-  { 
-    ignores: ['src/', 'test/', '.mocharc.js', 'config.js'] 
+  {
+    ignores: ['.mocharc.js', 'config.js']
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
